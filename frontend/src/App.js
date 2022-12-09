45ca79d08d26cbcd91d2d5be182bf33c73b61d7c
yenglianglai <ryanlai880122@gmail.com>
2022-12-09 15:59:01 +0800

    solve login routing problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import './index.css';
-import Login from './pages/Login';
-import MainPage from './pages/MainPage';
+import React, { useState, useEffect } from 'react'
+import './index.css'
+import Login from './pages/Login'
+import MainPage from './pages/MainPage'
 import Register from './pages/Register'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [login, setLogin] = useState(false);
-  // console.log(login)
+    const [login, setLogin] = useState(false)
 
-  return (
-    <>
-      <Router>
-        <Routes>
-          {/* {login ? <MainPage /> : <Login setLogin={setLogin} />} */}
-          {/* <Route path="/" element={login ? <MainPage /> : <Navigate path="/login" />} /> */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<Login login={login} setLogin={setLogin} />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
-
-    </>
-  )
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route
+                        path="/login"
+                        element={<Login login={login} setLogin={setLogin} />}
+                    />
+                    <Route
+                        path="/register"
+                        element={<Register setLogin={setLogin} />}
+                    />
+                </Routes>
+            </Router>
+        </>
+    )
 }
 
 export default App
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
@@ -6,98 +6,122 @@ import {
     MessageOutlined,
     TransactionOutlined,
     CoffeeOutlined,
-    SolutionOutlined
-} from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
-
-import BuyMe from './BuyMe';
-import MyTasks from './MyTasks';
-import Chat from './Chat';
-import Transfer from './Transfer';
+    SolutionOutlined,
+} from '@ant-design/icons'
+import { Layout, Menu } from 'antd'
+import { useApp } from '../UseApp'
+import BuyMe from './BuyMe'
+import MyTasks from './MyTasks'
+import Chat from './Chat'
+import Transfer from './Transfer'
 import Account from './Account'
+import { Navigate } from 'react-router-dom'
 
-const { Header, Sider, Content } = Layout;
+const { Header, Sider, Content } = Layout
 
 function MainPage() {
-    const [collapsed, setCollapsed] = useState(false);
-    const [key, setKey] = useState(1);
+    const [collapsed, setCollapsed] = useState(false)
+    const [key, setKey] = useState(1)
+    const { login } = useApp()
 
     function content() {
         switch (key) {
-            case "2":
+            case '2':
                 return <MyTasks></MyTasks>
-            case "3":
+            case '3':
                 return <Chat></Chat>
-            case "4":
+            case '4':
                 return <Transfer></Transfer>
-            case "5":
+            case '5':
                 return <Account></Account>
             default:
                 return <BuyMe></BuyMe>
         }
     }
 
-    return (
+    return login ? (
         <Layout>
-            <Sider trigger={null} collapsible collapsed={collapsed} style={{
-                overflow: 'auto', height: '100vh', left: 0, top:
-                    0, bottom: 0,
-            }}>
-                <div className="logo" style={{
-                    color: "white", display: "flex", alignItems: "center",
-                    justifyContent: "Center"
-                }}>
+            <Sider
+                trigger={null}
+                collapsible
+                collapsed={collapsed}
+                style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                }}
+            >
+                <div
+                    className="logo"
+                    style={{
+                        color: 'white',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'Center',
+                    }}
+                >
                     <h1>NTU BuyMe</h1>
                 </div>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} onClick={(e) => setKey(e.key)}
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    onClick={(e) => setKey(e.key)}
                     items={[
                         {
                             key: '1',
-                            icon:
-                                <CoffeeOutlined />,
+                            icon: <CoffeeOutlined />,
                             label: 'BuyMe',
                         },
                         {
                             key: '2',
-                            icon:
-                                <SolutionOutlined />,
+                            icon: <SolutionOutlined />,
                             label: 'MyTasks',
                         },
                         {
                             key: '3',
-                            icon:
-                                <MessageOutlined />,
+                            icon: <MessageOutlined />,
                             label: 'Chat',
                         },
                         {
                             key: '4',
-                            icon:
-                                <TransactionOutlined />,
+                            icon: <TransactionOutlined />,
                             label: 'Transfer',
                         },
                         {
                             key: '5',
-                            icon:
-                                <UserOutlined />,
+                            icon: <UserOutlined />,
                             label: 'Account',
                         },
                     ]}
                 />
             </Sider>
             <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }}>
-                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                        className: 'trigger',
-                        onClick: () => setCollapsed(!collapsed),
-                    })}
+                <Header
+                    className="site-layout-background"
+                    style={{ padding: 0 }}
+                >
+                    {React.createElement(
+                        collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                        {
+                            className: 'trigger',
+                            onClick: () => setCollapsed(!collapsed),
+                        }
+                    )}
                 </Header>
-                <Content className="site-layout-background" style={{ margin: '24px 16px', padding: 24, minHeight: 280, }}>
+                <Content
+                    className="site-layout-background"
+                    style={{ margin: '24px 16px', padding: 24, minHeight: 280 }}
+                >
                     {content()}
                 </Content>
             </Layout>
         </Layout>
-    );
-
+    ) : (
+        <Navigate to="/login" replace={true} />
+    )
 }
 
-export default MainPage;
+export default MainPage

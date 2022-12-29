8c735a723a72a205f6c8dd8b5118d848f62d0d1b
Lawrence Tsai <tsailawrence812@gmail.com>
2022-12-29 18:35:52 +0800

    update

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -7,40 +7,39 @@ import ChangePwdModal from './ChangePwdModal'
 import EditAccModal from './EditAccModal'
 const { Header, Content } = Layout
 
-
-
 const Account = ({ collapsed, setCollapsed, me }) => {
-    const [accountInfo, setAccountInfo] = useState('');
-    const [ChangePwdModalOpen, setChangePwdModalOpen] = useState(false);
-    const [EditAccModalOpen, setEditAccModalOpen] = useState(false);
-    const [onClickItem, setOnClickItem] = useState('');
-    
+    const [accountInfo, setAccountInfo] = useState('')
+    const [ChangePwdModalOpen, setChangePwdModalOpen] = useState(false)
+    const [EditAccModalOpen, setEditAccModalOpen] = useState(false)
+    const [onClickItem, setOnClickItem] = useState('')
+
     // Get Account
     useEffect(() => {
         getAccount(me)
+<<<<<<< Updated upstream
     });
 
+=======
+    })
+>>>>>>> Stashed changes
 
     const getAccount = async (user_id) => {
-        const {
-            data: account
-        } = 
-        await instance.get('/account',{
-        params : {
-            user_id,
-        }})
+        const { data: account } = await instance.get('/account', {
+            params: {
+                user_id,
+            },
+        })
         setAccountInfo(account.data)
     }
 
     // Edit Account
-    const editAccount = async(user_id, item, newValue) => {
-        const{
-            data: {message, content}
-        } =
-        await instance.post('/account',{
+    const editAccount = async (user_id, item, newValue) => {
+        const {
+            data: { message, content },
+        } = await instance.post('/account', {
             user_id,
             item,
-            newValue
+            newValue,
         })
     }
 
@@ -62,44 +61,47 @@ const Account = ({ collapsed, setCollapsed, me }) => {
         })
 
         switch (message) {
-        default:
-            break
-            
-        case 'error':
-            alert(content)
-            break
-        case 'success':{
-            // check current password
-            const result = bcrypt.compareSync(
-                value.user.currentpassword,
-                content.password
-            )
-            if (result) {
-                // check new password === retype new password
-                if(value.user.newpassword === value.user.newpasswordcheck){
-                    // update new password
-                    const newPasswordEncrypted = await encryptPassword(value.user.newpassword)
-                    const {
-                        data: { message, content },
-                    } = await instance.post('/changePassword', {
-                        user_id,
-                        newPasswordEncrypted
-                    })
-                    console.log(message)
-                    console.log(content)
+            default:
+                break
 
-                }else{
-                    window.alert('Retype new password does not match new password!')
+            case 'error':
+                alert(content)
+                break
+            case 'success': {
+                // check current password
+                const result = bcrypt.compareSync(
+                    value.user.currentpassword,
+                    content.password
+                )
+                if (result) {
+                    // check new password === retype new password
+                    if (
+                        value.user.newpassword === value.user.newpasswordcheck
+                    ) {
+                        // update new password
+                        const newPasswordEncrypted = await encryptPassword(
+                            value.user.newpassword
+                        )
+                        const {
+                            data: { message, content },
+                        } = await instance.post('/changePassword', {
+                            user_id,
+                            newPasswordEncrypted,
+                        })
+                        console.log(message)
+                        console.log(content)
+                    } else {
+                        window.alert(
+                            'Retype new password does not match new password!'
+                        )
+                    }
+                } else {
+                    window.alert('Current password is not correct!')
                 }
-
-            } else {
-                window.alert('Current password is not correct!')
+                break
             }
-            break}
         }
-
     }
-
 
     return (
         <Layout className="site-layout">
@@ -117,56 +119,89 @@ const Account = ({ collapsed, setCollapsed, me }) => {
                 style={{ margin: '24px 16px', padding: 24, minHeight: 280 }}
             >
                 <h1>Account</h1>
-                <List
-                    size="large"
-                    bordered
-                >
-                     <List.Item style={{display: 'flex', justifyContent: 'space-between'}}> 
-                        <Space direction='column' >
-                            <b>Student ID</b>{accountInfo.user_id}
-                        </Space>
-                    </List.Item>
-                    
-                    <List.Item style={{display: 'flex', justifyContent: 'space-between'}}> 
-                        <Space direction='column' align='center'>
-                            <b>Name</b>{accountInfo.name}
-                            <Button onClick={() =>{
-                                setEditAccModalOpen(true)
-                                setOnClickItem('name')}}>Edit</Button>
+                <List size="large" bordered>
+                    <List.Item
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <Space direction="column">
+                            <b>Student ID</b>
+                            {accountInfo.user_id}
                         </Space>
                     </List.Item>
 
-                    <List.Item style={{display: 'flex', justifyContent: 'space-between'}}> 
-                        <Space direction='column' align='center'>
-                            <b>Bank Account</b>{accountInfo.bankaccount}
-                            <Button onClick={() => {
-                                setEditAccModalOpen(true)
-                                setOnClickItem('bankaccount')}}>Edit</Button>
+                    <List.Item
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <Space direction="column" align="center">
+                            <b>Name</b>
+                            {accountInfo.name}
+                            <Button
+                                onClick={() => {
+                                    setEditAccModalOpen(true)
+                                    setOnClickItem('name')
+                                }}
+                            >
+                                Edit
+                            </Button>
                         </Space>
                     </List.Item>
-                   
+
+                    <List.Item
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <Space direction="column" align="center">
+                            <b>Bank Account</b>
+                            {accountInfo.bankaccount}
+                            <Button
+                                onClick={() => {
+                                    setEditAccModalOpen(true)
+                                    setOnClickItem('bankaccount')
+                                }}
+                            >
+                                Edit
+                            </Button>
+                        </Space>
+                    </List.Item>
+
                     <List.Item>
-                        <Button onClick={()=>setChangePwdModalOpen(true)}>Change Password</Button>
+                        <Button onClick={() => setChangePwdModalOpen(true)}>
+                            Change Password
+                        </Button>
                     </List.Item>
                 </List>
             </Content>
             <EditAccModal
-                user={accountInfo} 
-                open={EditAccModalOpen} 
-                onCreate={(values)=>{
-                    editAccount(me, Object.keys(values.user)[0], Object.values(values.user)[0])
-                    setEditAccModalOpen(false);
+                user={accountInfo}
+                open={EditAccModalOpen}
+                onCreate={(values) => {
+                    editAccount(
+                        me,
+                        Object.keys(values.user)[0],
+                        Object.values(values.user)[0]
+                    )
+                    setEditAccModalOpen(false)
                 }}
-                onCancel={()=>setEditAccModalOpen(false)}
-                item = {onClickItem}
+                onCancel={() => setEditAccModalOpen(false)}
+                item={onClickItem}
             />
-            <ChangePwdModal 
-                user_id={me} 
-                open={ChangePwdModalOpen} 
-                onCreate={(value)=>{
+            <ChangePwdModal
+                user_id={me}
+                open={ChangePwdModalOpen}
+                onCreate={(value) => {
                     changePassword(me, value)
-                    setChangePwdModalOpen(false)}}
-                onCancel={()=>setChangePwdModalOpen(false)}/>
+                    setChangePwdModalOpen(false)
+                }}
+                onCancel={() => setChangePwdModalOpen(false)}
+            />
         </Layout>
     )
 }

1b9756474fbb76c898247310d177f41e1818eee5
Lawrence Tsai <tsailawrence812@gmail.com>
2023-01-04 01:37:09 +0800

    style update

--- frontend/src/pages/NavBar.js
+++ frontend/src/pages/NavBar.js
@@ -16,7 +16,7 @@ const { Sider } = Layout
 
 function NavBar({ setKey, collapsed }) {
     const navigate = useNavigate()
-    const { setSignIn } = useApp()
+    const { setSignIn, setStatus } = useApp()
     function navigatePage(key) {
         setKey(key)
         switch (key) {
@@ -36,6 +36,10 @@ function NavBar({ setKey, collapsed }) {
                 //Logout
                 setSignIn(false)
                 navigate('/login')
+                setStatus({
+                    type: 'success',
+                    msg: 'Logout successfully!',
+                })
                 break
             default:
                 navigate('/')

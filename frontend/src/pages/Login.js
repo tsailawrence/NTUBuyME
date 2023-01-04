64254be8fa045b1825c05cc7c9b67c3710393d67
yenglianglai <ryanlai880122@gmail.com>
2023-01-05 01:00:36 +0800

    solve conflic

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -16,12 +16,8 @@ const Login = ({ setLogin, setCollapsed }) => {
         setSignIn,
         id,
         setId,
-<<<<<<< HEAD
         LOCALSTORAGE_ID_KEY,
         LOCALSTORAGE_NAME_KEY,
-=======
-        LOCALSTORAGE_KEY,
->>>>>>> fe7e700ed015ca0eb526812047d319e447680c31
         LOCALSTORAGE_STATUS,
     } = useApp()
     const [password, setPassword] = useState('')

d81ae39185b3f91cf5e202d0de187f597d018e97
Lawrence Tsai <tsailawrence812@gmail.com>
2023-01-06 14:24:00 +0800

    ws

--- frontend/src/UseApp.js
+++ frontend/src/UseApp.js
@@ -1,117 +1,116 @@
-import React, { createContext, useContext, useState, useEffect } from 'react'
-import { message } from 'antd'
-// import { ws } from './api'
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { message } from "antd";
 
 const AppContext = createContext({
-    status: {},
-    me: '',
-    signIn: false,
-})
-let portNum = window.location.port
-const c = new WebSocket(`wss://${window.location.host}:${portNum}`)
-c.onopen = () => console.log('Backend socket server connected!')
+  status: {},
+  me: "",
+  signIn: false,
+});
+let portNum = window.location.port;
+const c = new WebSocket(`wss://${window.location.host}:${portNum}`);
+c.onopen = () => console.log("Backend socket server connected!");
 
 const AppProvider = (props) => {
-    const [messages, setMessages] = useState([])
-    const [fulfill, setFulfill] = useState(false)
-    useState('')
-    const [status, setStatus] = useState([])
-    const [reconnect, setReconnnect] = useState(false)
-    const [signIn, setSignIn] = useState(false)
-    const [chats, setChats] = useState([])
-    const LOCALSTORAGE_ID_KEY = 'save-id'
-    const LOCALSTORAGE_NAME_KEY = 'save-me'
-    const LOCALSTORAGE_STATUS = 'status'
-    const savedId = localStorage.getItem(LOCALSTORAGE_ID_KEY)
-    const savedMe = localStorage.getItem(LOCALSTORAGE_NAME_KEY)
-    const [id, setId] = useState(savedId || '')
-    const [me, setMe] = useState(savedMe || '')
-    const [key, setKey] = useState('1')
+  const [messages, setMessages] = useState([]);
+  const [fulfill, setFulfill] = useState(false);
+  useState("");
+  const [status, setStatus] = useState([]);
+  const [reconnect, setReconnnect] = useState(false);
+  const [signIn, setSignIn] = useState(false);
+  const [chats, setChats] = useState([]);
+  const LOCALSTORAGE_ID_KEY = "save-id";
+  const LOCALSTORAGE_NAME_KEY = "save-me";
+  const LOCALSTORAGE_STATUS = "status";
+  const savedId = localStorage.getItem(LOCALSTORAGE_ID_KEY);
+  const savedMe = localStorage.getItem(LOCALSTORAGE_NAME_KEY);
+  const [id, setId] = useState(savedId || "");
+  const [me, setMe] = useState(savedMe || "");
+  const [key, setKey] = useState("1");
 
-    useEffect(() => {
-        displayStatus(status)
-    }, [status])
+  useEffect(() => {
+    displayStatus(status);
+  }, [status]);
 
-    ws.onmessage = (byteString) => {
-        const { data } = byteString
-        const [task, payload] = JSON.parse(data)
-        switch (task) {
-            case 'chat': {
-                setChats(payload)
-                break
-            }
+  c.onmessage = (byteString) => {
+    const { data } = byteString;
+    const [task, payload] = JSON.parse(data);
+    switch (task) {
+      case "chat": {
+        setChats(payload);
+        break;
+      }
 
-            case 'message': {
-                setMessages([...messages, payload])
-                break
-            }
+      case "message": {
+        setMessages([...messages, payload]);
+        break;
+      }
 
-            case 'fulfill': {
-                setFulfill(true)
-            }
-        }
+      case "fulfill": {
+        setFulfill(true);
+      }
     }
+  };
 
-    const displayStatus = (s) => {
-        if (s.msg) {
-            const { type, msg } = s
-            const status = {
-                content: msg,
-                duration: 1,
-            }
-            switch (type) {
-                case 'success':
-                    message.success(status)
-                    break
-                case 'error':
-                default:
-                    message.error(status)
-                    break
-            }
-        }
+  const displayStatus = (s) => {
+    if (s.msg) {
+      const { type, msg } = s;
+      const status = {
+        content: msg,
+        duration: 1,
+      };
+      switch (type) {
+        case "success":
+          message.success(status);
+          break;
+        case "error":
+        default:
+          message.error(status);
+          break;
+      }
     }
+  };
 
-    const sendData = (data) => {
-        if (ws.readyState === 1) {
-            ws.send(JSON.stringify(data))
-        } else {
-            //do something
-        }
+  const sendData = (data) => {
+    if (c.readyState === 1) {
+      c.send(JSON.stringify(data));
+    } else {
+      //do something
     }
+  };
 
-    const sendMessage = (me, body, chatBoxName) => {
-        sendData(['MESSAGE', { who: me, body: body, name: chatBoxName }])
-    }
+  const sendMessage = (me, body, chatBoxName) => {
+    sendData(["MESSAGE", { who: me, body: body, name: chatBoxName }]);
+  };
 
-    return (
-        <AppContext.Provider
-            value={{
-                me,
-                setMe,
-                id,
-                setId,
-                signIn,
-                setSignIn,
-                status,
-                setStatus,
-                messages,
-                setMessages,
-                LOCALSTORAGE_ID_KEY,
-                LOCALSTORAGE_NAME_KEY,
-                LOCALSTORAGE_STATUS,
-                sendData,
-                sendMessage,
-                chats,
-                setChats,
-                fulfill,
-                setFulfill,
-                key,
-                setKey,
-            }}
-            {...props}
-        />
-    )
-}
+  return (
+    <AppContext.Provider
+      value={{
+        me,
+        setMe,
+        id,
+        setId,
+        signIn,
+        setSignIn,
+        status,
+        setStatus,
+        messages,
+        setMessages,
+        LOCALSTORAGE_ID_KEY,
+        LOCALSTORAGE_NAME_KEY,
+        LOCALSTORAGE_STATUS,
+        sendData,
+        sendMessage,
+        chats,
+        setChats,
+        fulfill,
+        setFulfill,
+        key,
+        setKey,
+      }}
+      {...props}
+    />
+  );
+};
 
-const useApp = () => useContext(AppContext)
-export { AppProvider, useApp }
+const useApp = () => useContext(AppContext);
+export { AppProvider, useApp };
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,10 +6,3 @@ const API_ROOT =
     : "http://localhost:4000/api";
 
 export const instance = axios.create({ baseURL: API_ROOT });
-
-const WS_URL =
-  process.env.NODE_ENV === "production"
-    ? window.location.origin.replace(/^http/, "ws")
-    : "ws://localhost:8080";
-
-export const ws = new WebSocket(WS_URL);
